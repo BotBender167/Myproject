cad153e2689ad5b83533003aa1ab7203eb0b830b
Anmol poddar <podderneha80@gmail.com>
2026-04-19 13:09:45 +0530

    Fix CORS for Vercel

--- backend/main.py
+++ backend/main.py
@@ -36,9 +36,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173", 
+        "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://myproject-xbsg-4wyzka7bo-botbender167s-projects.vercel.app"
+        "https://myproject-61cz20ed1-botbender167s-projects.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
